8b8afc0e47c0d8e79b491129f979055dce20e18b
alexandrafriedman <alextfriedman@gmail.com>
2022-05-25 13:09:01 -0400

    added scroll animations

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ hamburger.addEventListener("click", toggleMenu);
 menuItems.forEach((menuItem) => {
     menuItem.addEventListener("click", toggleMenu)
 });
+
+// Scroll animation
+
+const reveal = () => {
+    let reveals = document.querySelectorAll(".reveal")
+    
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
